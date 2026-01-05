b5e5cfb923a4aa01f928f03158cbd9546ed677d8
rhulraj <rahulanku51@gmail.com>
2026-01-05 20:51:35 +0530

    completed all modeule

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,11 +1,12 @@
 import os
 
 AWS_S3_BUCKET_NAME = "wafer-fault"
-MONGO_DATABASE_NAME = "pwskills"
-MONGO_COLLECTION_NAME = "waterfault"
+MONGO_DATABASE_NAME = "rahulraj"
+MONGO_COLLECTION_NAME = "waferfault"
 
 TARGET_COLLUMN = "quality"
-MONGO_DB_URL = "mongodb+srv://snshrivas:Snshrivaas@cluster0.u46c4.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL = "mongodb+srv://rahulraj:software.mdb@cluster0.mwjyvs2.mongodb.net/?appName=Cluster0"
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
+artifact_folder = 'artifacts'
